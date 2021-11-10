ddb182c372b400c80c775f269aa6f9e6fd8d463c
gabriel-rodriguez-ruiz <42478425+gabriel-rodriguez-ruiz@users.noreply.github.com>
2021-11-10 07:15:45 -0300

    Nueva versión de Luis
    Se agregaron algunas funciones para calcular si se cumple con los requisitos de variacion de temperatura en una hora y doce horas.

--- DataPython/densidades.py
+++ DataPython/densidades.py
@@ -10,7 +10,37 @@ import statistics
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+from datetime import datetime
 
+#%% defino las condiciones de variacion maxima de temperatura y humedad para cada clase de pesa (de la R11)
+#   por defecto queda seleccionada la clase m1
+clase='m1'
+if clase=='e1':
+    deltaMaxTemHora=0.3
+    deltaMaxTemdoceHoras=0.5
+    deltaMaxHum=5
+elif clase=='e2':
+    deltaMaxTemHora=0.7
+    deltaMaxTemdoceHoras=1
+    deltaMaxHum=10
+elif clase=='f1':
+    deltaMaxTemHora=1.5
+    deltaMaxTemdoceHoras=2
+    deltaMaxHum=15
+elif clase=='f2':
+    deltaMaxTemHora=2
+    deltaMaxTemdoceHoras=3.5
+    deltaMaxHum=15    
+else:
+    deltaMaxTemHora=3
+    deltaMaxTemdoceHoras=5
+    deltaMaxHum=100      
+    
+    
+#%% en este bloque agregue la lista tiempo, en la que se define el tiempo en segundos en que se realiza cada medicion
+   # usando la funcion timestamp()
+    
+    
 with open('../DataPython/202109-06.csv') as File:
     reader = csv.reader(File, delimiter=';')
     hora=[]
@@ -18,12 +48,20 @@ with open('../DataPython/202109-06.csv') as File:
     presion=[]
     humedad=[]
     densidad=[]
+    fecha=[]
+    tiempo=[]
     for i,line in enumerate(reader):
-        hora.append(line[18])
+        listahora=line[18].split(':')
+        listafecha=line[17].split('/')
+        tiempo.append(datetime(int(listafecha[2]),int(listafecha[1]),int(listafecha[0]),int(listahora[0]),int(listahora[1]), int(listahora[2])).timestamp())
+        hora.append(line[18].split(':'))
+        fecha.append(line[17].split('/'))
         temperatura.append(float(line[1]))
         humedad.append(float(line[15]))
         presion.append(float(line[16]))
         densidad.append((0.34848*float(line[16])-0.009*float(line[15])*math.exp(0.061*float(line[1])))/(273.15+float(line[1])))    
+
+    #First Term => CIPM 1981/91: 0.34848      CIPM 2007:0.3483740
     print('la temperatura es: \n',temperatura)
     print('la presion es: \n', presion)
     print('la hora es: \n',hora)
@@ -44,7 +82,6 @@ plt.ylim(min(densidad)-0.0003 , max(densidad)+0.0003 )
 plt.plot(densidad)
 plt.annotate(f'{hora[indMaxHora]}',xy=(indMaxHora,max(densidad)+0.0001))
 plt.annotate(f'{hora[indMinHora]}',xy=(indMinHora,min(densidad)-0.0001))
-print(hora[indMaxHora])
 
 #%% Grafico Temperatura
 plt.figure(figsize=(10, 8), dpi=80)
@@ -91,3 +128,72 @@ plt.title('Presion vs Tiempo')
 plt.ylim(min(presion)-1 , max(presion)+1 )
 plt.annotate(f'{hora[indMaxHoraPre]}',xy=(indMaxHoraPre,max(presion)+0.2))
 plt.annotate(f'{hora[indMinHoraPre]}',xy=(indMinHoraPre,min(presion)-0.2))
+
+
+
+
+#%% En este bloque se calculan las variables iHora e idoceHoras, indican la cantidad de mediciones que se hacen 
+#   para alcanzar una hora y doce horas, respectivamente. Esta variables despues se van a usar en las funciones
+#   verificarTempHora y verificarTempdoceHoras 
+iHora=0
+unaHora=0
+while unaHora<1:
+    iHora+=1
+    unaHora=(tiempo[iHora]-tiempo[0])/3600
+idoceHoras=0
+doceHoras=0
+while doceHoras<12:
+    idoceHoras+=1
+    doceHoras=(tiempo[idoceHoras]-tiempo[0])/3600
+
+#%%
+def verificarTempHora(lista):
+    '''recibe la lista de temperaturas, calcula la variacion maxima que se produce en la primer
+    hora de mediciones y verifica si supera o no el maximo permitido
+    en caso de cumplir, elimina el primer elemento de la lista y vuelve a verificar de manera recursiva.
+    por ultimo, imprime en pantalla si cumple o no con dicho requisito'''
+    delta=max(lista[:iHora])-min(lista[:iHora])
+    if delta>deltaMaxTemHora:
+        print('hay exceso de variacion de temperatura en una hora')
+    elif len(lista)<iHora:
+        print('no hay exceso de variacion de temperatura en una hora')
+    else:
+        lista.pop(0)
+        verificarTempHora(lista)
+        
+def verificarTempdoceHoras(lista):
+    '''idem funcion verificarTempHora, la diferencia es que en lugar de una hora en este caso son 12 horas'''
+    
+    delta=max(lista[:idoceHoras])-min(lista[:idoceHoras])
+    if delta>deltaMaxTemdoceHoras:
+        print('hay exceso de variacion de temperatura en doce horas')
+    elif len(lista)<idoceHoras:
+        print('no hay exceso de variacion de temperatura en doce horas')
+    else:
+        lista.pop(0)
+        verificarTempdoceHoras(lista)
+
+
+print()
+#clase='e1'
+verificarTempHora(temperatura)
+verificarTempdoceHoras(temperatura)
+
+
+#%% Esta funcion todavia no funciona, estuve viendo si podia hacer una funcion que haga todos los graficos
+    # no funciona porque no puedo modificar lo que imprime plt.ylabel ni tampoco plt.title, trate de usar 
+    # f-strings pero no sirvio
+    
+def grafico(columna):
+    plt.figure(figsize=(10, 8), dpi=80)
+    plt.plot(columna)
+    indMaxHoraTem=columna.index(max(columna))
+    indMinHoraTem=columna.index(min(columna))
+    plt.xticks([indMaxHoraTem,indMinHoraTem])
+    plt.yticks([min(columna),max(columna),statistics.mean(columna)])
+    plt.xlabel('Tiempo')
+    plt.ylabel(f'{columna}')
+    plt.title('Temperatura vs Tiempo')
+    plt.ylim(min(columna)-0.1 , max(columna)+0.1 )
+    plt.annotate(f'{hora[indMaxHoraTem]}',xy=(indMaxHoraTem,max(columna)+0.05))
+    plt.annotate(f'{hora[indMinHoraTem]}',xy=(indMinHoraTem,min(columna)-0.05))
