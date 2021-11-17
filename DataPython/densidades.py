e5f01569b6aae18c091b101a9a2426636049fcbf
gabriel-rodriguez-ruiz <42478425+gabriel-rodriguez-ruiz@users.noreply.github.com>
2021-11-17 17:28:49 -0300

    Actualización de Luis
    Nuevas funciones: verificaciones y graficos

--- DataPython/densidades.py
+++ DataPython/densidades.py
@@ -1,56 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Oct  1 09:38:41 2021
+Created on Tue Nov 16 09:08:27 2021
 
-@author: INTI
+@author: ltouc
 """
 
+import pandas as pd
 import csv
 import statistics
 import math
-import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-#%% defino las condiciones de variacion maxima de temperatura y humedad para cada clase de pesa (de la R11)
-#   por defecto queda seleccionada la clase m1
-clase='m1'
-if clase=='e1':
-    deltaMaxTemHora=0.3
-    deltaMaxTemdoceHoras=0.5
-    deltaMaxHum=5
-elif clase=='e2':
-    deltaMaxTemHora=0.7
-    deltaMaxTemdoceHoras=1
-    deltaMaxHum=10
-elif clase=='f1':
-    deltaMaxTemHora=1.5
-    deltaMaxTemdoceHoras=2
-    deltaMaxHum=15
-elif clase=='f2':
-    deltaMaxTemHora=2
-    deltaMaxTemdoceHoras=3.5
-    deltaMaxHum=15    
-else:
-    deltaMaxTemHora=3
-    deltaMaxTemdoceHoras=5
-    deltaMaxHum=100      
-    
-class Clase:
-    """Clase de precisión del INTI."""
-    def __init__(self, nombre, deltaMaxTemHora, deltaMaxTemdoceHoras, deltaMaxHum):
-        self.nombre = nombre
-        self.deltaMaxTemHora = deltaMaxTemHora
-        self.deltaMaxTemdoceHoras = deltaMaxTemdoceHoras
-        self.deltaMaxHum = deltaMaxHum
-    def __str__(self):
-        return f"{self.nombre}"
-    def __repr__(self):
-        return f"Clase({self.nombre}, {self.deltaMaxTemHora}, {self.deltaMaxTemdoceHoras}, {self.deltaMaxHum})"
-    
-#%% en este bloque agregue la lista tiempo, en la que se define el tiempo en segundos en que se realiza cada medicion
-   # usando la funcion timestamp()
-    
+
+#%% Funciones
+
 def leer_csv(nombre_archivo):
     """
     Lee archivo csv 'nombre_archivo' y devuelve listas con el contenido
@@ -73,159 +37,18 @@ def leer_csv(nombre_archivo):
             hora.append(line[18].split(':'))
             fecha.append(line[17].split('/'))
             fecha_hora.append(datetime(*list(map(int,line[17].split("/")[::-1]+line[18].split(":")))))
-            temperatura.append(float(line[1]))
+            temperatura.append((float(line[0])+float(line[1])+float(line[2])+float(line[4])+float(line[5])+float(line[6])+float(line[7])+float(line[9])+float(line[10])+float(line[13])+float(line[14]))/11)
             humedad.append(float(line[15]))
             presion.append(float(line[16]))
             densidad.append((0.34848*float(line[16])-0.009*float(line[15])*math.exp(0.061*float(line[1])))/(273.15+float(line[1])))    
     return fecha_hora, hora, temperatura, presion, humedad, densidad, tiempo
 
-fecha_hora, hora, temperatura, presion, humedad, densidad, tiempo = leer_csv('../DataPython/202109-06.csv')
-#First Term => CIPM 1981/91: 0.34848      CIPM 2007:0.3483740
-print('la temperatura es: \n',temperatura)
-print('la presion es: \n', presion)
-print('la hora es: \n', fecha_hora)
-print('la humedad es: \n', humedad) 
-print('La densidad es: \n', densidad)
-
-#%% Grafico Densidad
-indMaxHora=densidad.index(max(densidad))
-indMinHora=densidad.index(min(densidad))
-plt.figure(figsize=(10, 8), dpi=80)
-plt.xticks([indMaxHora,indMinHora])
-plt.yticks([min(densidad),max(densidad),statistics.mean(densidad)])
-#plt.xlim(X.min() * 1.1, X.max() * 1.1)
-plt.xlabel('Tiempo')
-plt.ylabel('Densidad')
-plt.title('Densidad vs Tiempo')
-plt.ylim(min(densidad)-0.0003 , max(densidad)+0.0003 )
-plt.plot(densidad)
-plt.annotate(f'{hora[indMaxHora]}',xy=(indMaxHora,max(densidad)+0.0001))
-plt.annotate(f'{hora[indMinHora]}',xy=(indMinHora,min(densidad)-0.0001))
-
-#%% Grafico Temperatura
-plt.figure(figsize=(10, 8), dpi=80)
-plt.plot(temperatura)
-indMaxHoraTem=temperatura.index(max(temperatura))
-indMinHoraTem=temperatura.index(min(temperatura))
-plt.xticks([indMaxHoraTem,indMinHoraTem])
-plt.yticks([min(temperatura),max(temperatura),statistics.mean(temperatura)])
-plt.xlabel('Tiempo')
-plt.ylabel('Temperatura')
-plt.title('Temperatura vs Tiempo')
-plt.ylim(min(temperatura)-0.1 , max(temperatura)+0.1 )
-plt.annotate(f'{hora[indMaxHoraTem]}',xy=(indMaxHoraTem,max(temperatura)+0.05))
-plt.annotate(f'{hora[indMinHoraTem]}',xy=(indMinHoraTem,min(temperatura)-0.05))
-
-#%% Grafico Humedad
-plt.figure(figsize=(10, 8), dpi=80)
-plt.plot(humedad)
-indMaxHoraHum=humedad.index(max(humedad))
-indMinHoraHum=humedad.index(min(humedad))
-plt.xticks([indMaxHoraHum,indMinHoraHum])
-plt.yticks([min(humedad),max(humedad),statistics.mean(humedad)])
-plt.xlabel('Tiempo')
-plt.ylabel('Humedad')
-plt.title('Humedad vs Tiempo')
-plt.ylim(min(humedad)-1 , max(humedad)+1 )
-plt.annotate(f'{hora[indMaxHoraHum]}',xy=(indMaxHoraHum,max(humedad)+0.05))
-plt.annotate(f'{hora[indMinHoraHum]}',xy=(indMinHoraHum,min(humedad)-0.05))
-if min(humedad)<40:
-    plt.annotate(f'{hora[indMinHoraHum]} no cumple Humedad Minima',xy=(indMinHoraHum,min(humedad)-0.3))
-if max(humedad)>60:
-    plt.annotate(f'{hora[indMaxHoraHum]} no cumple Humedad Maxima',xy=(indMaxHoraHum,max(humedad)+0.3))
-    
-#%% Grafico Presion
-plt.figure(figsize=(10, 8), dpi=80)
-plt.plot(presion)
-indMaxHoraPre=presion.index(max(presion))
-indMinHoraPre=presion.index(min(presion))
-plt.xticks([indMaxHoraPre,indMinHoraPre])
-plt.yticks([min(presion),max(presion),statistics.mean(presion)])
-plt.xlabel('Tiempo')
-plt.ylabel('presion')
-plt.title('Presion vs Tiempo')
-plt.ylim(min(presion)-1 , max(presion)+1 )
-plt.annotate(f'{hora[indMaxHoraPre]}',xy=(indMaxHoraPre,max(presion)+0.2))
-plt.annotate(f'{hora[indMinHoraPre]}',xy=(indMinHoraPre,min(presion)-0.2))
-
-
-
-
-#%% En este bloque se calculan las variables iHora e idoceHoras, indican la cantidad de mediciones que se hacen 
-#   para alcanzar una hora y doce horas, respectivamente. Esta variables despues se van a usar en las funciones
-#   verificarTempHora y verificarTempdoceHoras 
-iHora=0
-unaHora=0
-while unaHora<1:
-    iHora+=1
-    unaHora=(tiempo[iHora]-tiempo[0])/3600
-idoceHoras=0
-doceHoras=0
-while doceHoras<12:
-    idoceHoras+=1
-    doceHoras=(tiempo[idoceHoras]-tiempo[0])/3600
-
-#%%
-def verificarTempHora(lista):
-    '''recibe la lista de temperaturas, calcula la variacion maxima que se produce en la primer
-    hora de mediciones y verifica si supera o no el maximo permitido
-    en caso de cumplir, elimina el primer elemento de la lista y vuelve a verificar de manera recursiva.
-    por ultimo, imprime en pantalla si cumple o no con dicho requisito'''
-    delta=max(lista[:iHora])-min(lista[:iHora])
-    if delta>deltaMaxTemHora:
-        print('hay exceso de variacion de temperatura en una hora')
-    elif len(lista)<iHora:
-        print('no hay exceso de variacion de temperatura en una hora')
-    else:
-        lista.pop(0)
-        verificarTempHora(lista)
-        
-def verificarTempdoceHoras(lista):
-    '''idem funcion verificarTempHora, la diferencia es que en lugar de una hora en este caso son 12 horas'''
-    
-    delta=max(lista[:idoceHoras])-min(lista[:idoceHoras])
-    if delta>deltaMaxTemdoceHoras:
-        print('hay exceso de variacion de temperatura en doce horas')
-    elif len(lista)<idoceHoras:
-        print('no hay exceso de variacion de temperatura en doce horas')
-    else:
-        lista.pop(0)
-        verificarTempdoceHoras(lista)
-
-
-print()
-#clase='e1'
-verificarTempHora(temperatura)
-verificarTempdoceHoras(temperatura)
-
-
-#%% Esta funcion todavia no funciona, estuve viendo si podia hacer una funcion que haga todos los graficos
-    # no funciona porque no puedo modificar lo que imprime plt.ylabel ni tampoco plt.title, trate de usar 
-    # f-strings pero no sirvio
-    
-def grafico(columna):
-    plt.figure(figsize=(10, 8), dpi=80)
-    plt.plot(columna)
-    indMaxHoraTem=columna.index(max(columna))
-    indMinHoraTem=columna.index(min(columna))
-    plt.xticks([indMaxHoraTem,indMinHoraTem])
-    plt.yticks([min(columna),max(columna),statistics.mean(columna)])
-    plt.xlabel('Tiempo')
-    plt.ylabel(f'{columna}')
-    plt.title('Temperatura vs Tiempo')
-    plt.ylim(min(columna)-0.1 , max(columna)+0.1 )
-    plt.annotate(f'{hora[indMaxHoraTem]}',xy=(indMaxHoraTem,max(columna)+0.05))
-    plt.annotate(f'{hora[indMinHoraTem]}',xy=(indMinHoraTem,min(columna)-0.05))
-
-
-#%% Manejo de csv con Pandas    
-import pandas as pd
+# Manejo de csv con Pandas    
 
 def mediciones_durante_calibracion(inicio, final, nombre_archivo1, nombre_archivo2):
     """
     Devuelve un DataFrame a partir de dos archivos csv con los datos restringidos
     a las horas de 'inicio' y 'final'.    
-
     Parameters
     ----------
     inicio : datetime.datetime
@@ -236,7 +59,6 @@ def mediciones_durante_calibracion(inicio, final, nombre_archivo1, nombre_archiv
         Ruta del primer archivo csv.
     nombre_archivo2 : str
         Ruta del segundo archivo csv.
-
     Returns
     -------
     df : pandas.DataFrame
@@ -249,12 +71,193 @@ def mediciones_durante_calibracion(inicio, final, nombre_archivo1, nombre_archiv
     df2 = pd.DataFrame(zip(fecha_hora2, temperatura2, presion2, humedad2, densidad2, tiempo2),
                        columns = columnas)
     df = pd.concat([df1, df2], ignore_index = True)      #Concatenamos los DataFrames
-    df.set_index('Hora')
     df = df[(df["Hora"] >= inicio) & (df["Hora"]<= final)]
     return df
+
+
+def verificaciones(dfTiempo, dfTemp, dfHum, dfDen, clase):
+    '''Esta funcion imprime en pantalla si se cumple o no con los requerimientos de la normativa OIML R111-1
     
+    Pre: recibe las columnas del dataFrame correspondientes al timestamp, temperatura, humedad y densidad
+         y la clase de pesa en formato string ('e1', 'e2', 'f1', 'f2' o 'm1')
+    Pos: la funcion no retorna nada, solo imprime los resultados en pantalla'''
+    
+    # Definicion de Clase de Pesa
+    if clase=='e1':
+        deltaMaxTemHora=0.3
+        deltaMaxTemdoceHoras=0.5
+        deltaMaxHum=5
+    elif clase=='e2':
+        deltaMaxTemHora=0.7
+        deltaMaxTemdoceHoras=1
+        deltaMaxHum=10
+    elif clase=='f1':
+        deltaMaxTemHora=1.5
+        deltaMaxTemdoceHoras=2
+        deltaMaxHum=15
+    elif clase=='f2':
+        deltaMaxTemHora=2
+        deltaMaxTemdoceHoras=3.5
+        deltaMaxHum=15    
+    elif clase=='m1':
+        deltaMaxTemHora=3
+        deltaMaxTemdoceHoras=5
+        deltaMaxHum=100  
+        
+    print('\nRequerimientos OIML R111-1:\n')
+   
+    if min(dfDen)<(1.2*0.9): 
+        print(f'Se excedio el limite inferior de densidad, ya que la minima densidad registrada fue: {min(dfDen)} ')
+    else:
+        print('No se excedio el limite inferior de densidad')
+    if max(dfDen)>(1.2*1.1):
+        print(f'Se excedio el limite superior de densidad, ya que la maxima densidad registrada fue: {max(dfDen)}')
+    else:
+        print('No se excedio el limite superior de densidad')
+    if min(dfHum)<=40:
+        print(f'Se excedio el minimo de humedad, la minima humedad registrada fue: {min(dfHum)}%')
+    else:
+        print('No se excedio el minimo de humedad')
+    
+    if max(dfHum)>=60:
+        print(f'Se excedio el maximo de humedad, la maxima humedad registrada fue: {max(dfHum)}%')
+    else:
+        print('No se excedio el maximo de humedad')  
+        
+    if (clase=='e1' or clase=='e2') and min(dfTemp)<=18:
+        print(f'Se excedio el minimo de temperatura, la minima temperatura registrada fue: {min(dfTemp)}')
+        
+    if (clase=='e1' or clase=='e2') and max(dfTemp)>=27:
+        print(f'Se excedio el maximo de temperatura, la maxima temperatura registrada fue: {max(dfTemp)}')
+              
+    primerIndice=dfTiempo.index[0]    
+    iHora=0    # iHora es la cantidad de mediciones que se hacen en una hora 
+    unaHora=0
+    while unaHora<1:
+        iHora+=1
+        unaHora=(dfTiempo[primerIndice+iHora]-dfTiempo[primerIndice])/3600    
+    
+    iCuatroHoras=0  #iCuatroHoras es la cantidad de mediciones que se hacen en 4 horas
+    cuatroHoras=0
+    while cuatroHoras<4:
+        iCuatroHoras+=1
+        cuatroHoras=(dfTiempo[primerIndice+iCuatroHoras]-dfTiempo[primerIndice])/3600    
+    
+    idoceHoras=0
+    doceHoras=0
+    while doceHoras<12:
+        idoceHoras+=1
+        doceHoras=(dfTiempo[primerIndice+idoceHoras]-dfTiempo[primerIndice])/3600       
+  
+    lista=list(dfTemp)
+    while True:
+        delta=max(lista[:iHora])-min(lista[:iHora])
+        if delta>deltaMaxTemHora:
+            print(f'Hay exceso de variacion de temperatura en una hora, se registro una variacion de {delta}')
+            break
+        elif len(lista)<iHora:
+            print('No hay exceso de variacion de temperatura en una hora')
+            break
+        else:
+            lista.pop(0)
+    
+    lista=list(dfTemp)
+    while True:
+        delta=max(lista[:idoceHoras])-min(lista[:idoceHoras])
+        if delta>deltaMaxTemdoceHoras:
+            print(f'Hay exceso de variacion de temperatura en doce horas, se registro una variacion de: {delta}')
+            break
+        elif len(lista)<idoceHoras:
+            print('No hay exceso de variacion de temperatura en doce horas')
+            break
+        else:
+            lista.pop(0)   
+  
+    lista=list(dfHum)
+    while True:
+        delta=max(lista[:iCuatroHoras])-min(lista[:iCuatroHoras])
+        if delta>deltaMaxHum:
+            print(f'Hay exceso de variacion de humedad en cuatro horas, se registro: {delta}')
+            break
+        elif len(lista)<iCuatroHoras:
+            print('No hay exceso de variacion de humedad en cuatro horas')
+            break
+        else:
+            lista.pop(0)
+ 
+# Graficos 
+def graficos(densidad,temperatura,humedad,presion):
+    '''Grafica la densidad, temperatura, humedad y presion en funcion del tiempo
+    sobre el eje de ordenadas indica el valor maximo, medio y minimo 
+    indica en el grafico en que horario se produjeron los valores maximos y minimos de 
+    la variable que esta midiendo
+    
+    Pre: recibe los valores de densidad, temperatura, humedad y presion en forma de lista
+    Pos: No devuelve nada, solo realiza los graficos'''
+    # Grafico Densidad
+    indMaxHora=densidad.index(max(densidad))
+    indMinHora=densidad.index(min(densidad))
+    plt.figure(figsize=(10, 8), dpi=80)
+    plt.xticks([indMaxHora,indMinHora])
+    plt.yticks([min(densidad),max(densidad),statistics.mean(densidad)])
+    plt.xlabel('Tiempo')
+    plt.ylabel('Densidad')
+    plt.title('Densidad vs Tiempo')
+    plt.ylim(min(densidad)-0.0003 , max(densidad)+0.0003 )
+    plt.plot(densidad)
+    plt.annotate(f'{hora[indMaxHora][0]}:{hora[indMaxHora][1]}:{hora[indMaxHora][2]}',xy=(indMaxHora,max(densidad)+0.0001))
+    plt.annotate(f'{hora[indMinHora][0]}:{hora[indMinHora][1]}:{hora[indMinHora][2]}',xy=(indMinHora,min(densidad)-0.0001))
+
+    # Grafico Temperatura
+    plt.figure(figsize=(10, 8), dpi=80)
+    plt.plot(temperatura)
+    indMaxHoraTem=temperatura.index(max(temperatura))
+    indMinHoraTem=temperatura.index(min(temperatura))
+    plt.xticks([indMaxHoraTem,indMinHoraTem])
+    plt.yticks([min(temperatura),max(temperatura),statistics.mean(temperatura)])
+    plt.xlabel('Tiempo')
+    plt.ylabel('Temperatura')
+    plt.title('Temperatura vs Tiempo')
+    plt.ylim(min(temperatura)-0.1 , max(temperatura)+0.1 )
+    plt.annotate(f'{hora[indMaxHoraTem][0]}:{hora[indMaxHoraTem][1]}:{hora[indMaxHoraTem][2]}',xy=(indMaxHoraTem,max(temperatura)+0.05))
+    plt.annotate(f'{hora[indMinHoraTem][0]}:{hora[indMinHoraTem][1]}:{hora[indMinHoraTem][2]}',xy=(indMinHoraTem,min(temperatura)-0.05))
+
+    # Grafico Humedad
+    plt.figure(figsize=(10, 8), dpi=80)
+    plt.plot(humedad)
+    indMaxHoraHum=humedad.index(max(humedad))
+    indMinHoraHum=humedad.index(min(humedad))
+    plt.xticks([indMaxHoraHum,indMinHoraHum])
+    plt.yticks([min(humedad),max(humedad),statistics.mean(humedad)])
+    plt.xlabel('Tiempo')
+    plt.ylabel('Humedad')
+    plt.title('Humedad vs Tiempo')
+    plt.ylim(min(humedad)-1 , max(humedad)+1 )
+    plt.annotate(f'{hora[indMaxHoraHum][0]}:{hora[indMaxHoraHum][1]}:{hora[indMaxHoraHum][2]}',xy=(indMaxHoraHum,max(humedad)+0.05))
+    plt.annotate(f'{hora[indMinHoraHum][0]}:{hora[indMinHoraHum][1]}:{hora[indMinHoraHum][2]}',xy=(indMinHoraHum,min(humedad)-0.05))
+
+    # Grafico Presion
+    plt.figure(figsize=(10, 8), dpi=80)
+    plt.plot(presion)
+    indMaxHoraPre=presion.index(max(presion))
+    indMinHoraPre=presion.index(min(presion))
+    plt.xticks([indMaxHoraPre,indMinHoraPre])
+    plt.yticks([min(presion),max(presion),statistics.mean(presion)])
+    plt.xlabel('Tiempo')
+    plt.ylabel('Presion')
+    plt.title('Presion vs Tiempo')
+    plt.ylim(min(presion)-1 , max(presion)+1 )
+    plt.annotate(f'{hora[indMaxHoraPre][0]}:{hora[indMaxHoraPre][1]}:{hora[indMaxHoraPre][2]}',xy=(indMaxHoraPre,max(presion)+0.2))
+    plt.annotate(f'{hora[indMinHoraPre][0]}:{hora[indMinHoraPre][1]}:{hora[indMinHoraPre][2]}',xy=(indMinHoraPre,min(presion)-0.2))            
+    
+
+    
+#%% Ejecucion de Funciones
+fecha_hora, hora, temperatura, presion, humedad, densidad, tiempo = leer_csv('../DataPython/202109-06.csv')
 nombre_archivo1 = "../DataPython/202109-05.csv"
 nombre_archivo2 = '../DataPython/202109-06.csv'
 inicio = datetime(2021, 9, 5, 8, 0, 0)
 final = datetime(2021, 9, 6, 8, 0, 0)
 df = mediciones_durante_calibracion(inicio, final, nombre_archivo1, nombre_archivo2)
+verificaciones(df["Tiempo"], df["Temperatura"], df["Humedad"],df["Densidad"],'e1')
+graficos(densidad,temperatura,humedad,presion)
